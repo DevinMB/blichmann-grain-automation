3d1f06b1cb7f733d73f4b739b45dc9e1835ebd26
Devin Butts <devinbutts@Devins-MacBook-Pro.local>
2024-07-21 19:10:05 -0400

    pins

--- button-relay-test.py
+++ button-relay-test.py
@@ -21,7 +21,7 @@ override_switch = Button(OVERRIDE_PIN)
 # Set up the NeoPixel LED
 # pixels = neopixel.NeoPixel(LED_PIN, NUM_LEDS)
 pixels = neopixel.NeoPixel(
-    pixel_pin=LED_PIN, num_pixels=NUM_LEDS, brightness=0.2, auto_write=True, pixel_order=ORDER
+    pin=LED_PIN, n=NUM_LEDS, brightness=0.2, auto_write=True, pixel_order=ORDER
 )
 
 def turn_off_relay(option=None):
