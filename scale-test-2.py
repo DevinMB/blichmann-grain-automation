c67aaf89e2dd8dbb6fe18e4784a0963b7eacfe8d
Devin Butts <devinbutts@Devins-MacBook-Pro.local>
2024-07-25 13:47:34 -0400

    .

--- scale-test-2.py
+++ scale-test-2.py
@@ -39,10 +39,10 @@ try:
         val = hx.get_weight(1)
         print(f'Weight: {val:.2f} LBS')
         
-        # hx.power_down()
-        # time.sleep(0.5)  # Ensure the sensor has time to power down
-        # hx.power_up()
-        # time.sleep(.7)  # Delay between readings to allow the sensor to stabilize
+        hx.power_down()
+        time.sleep(0.5)  # Ensure the sensor has time to power down
+        hx.power_up()
+        time.sleep(.7)  # Delay between readings to allow the sensor to stabilize
 except (KeyboardInterrupt, SystemExit):
     # Cleanup GPIO on exit
     GPIO.cleanup()
