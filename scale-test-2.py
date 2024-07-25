1114f18e05deee586024cd49283e682841f9e65e
Devin Butts <devinmbutts@gmail.com>
2024-07-25 19:54:31 -0400

    .

--- scale-test-2.py
+++ scale-test-2.py
@@ -1,46 +1,41 @@
 import RPi.GPIO as GPIO
 from hx711 import HX711
 import time
-
 # Define GPIO pins
 DT_PIN = 6   
 SCK_PIN = 5  
-
 # Initialize HX711
 hx = HX711(DT_PIN, SCK_PIN)
-
-time.sleep(.5)
-print(f"zeroing scale")
-
 # Tare to zero
 hx.tare()
 
 # Place a known weight (10 lbs = 4535.92 grams) on the load cell and get raw data
-reference_unit = -1335.4097777777752  # Your previously calculated reference unit
+reference_unit = 9944.223777777794  # Your previously calculated reference unit
 hx.set_reference_unit(reference_unit)
 # For Grams : 21.926201667283962
 # For Pounds: 9944.223777777794
-
+# Reset the HX711
 hx.reset()
+# Tare to zero again after setting reference unit
 hx.tare()
-
-hx.power_down()
-time.sleep(0.5)  # Ensure the sensor has time to power down
-hx.power_up()
-time.sleep(1)  # Delay between readings to allow the sensor to stabilize
-
-print(f"starting reads..")
-
+def get_stable_weight(num_readings=10):
+    weights = []
+    for _ in range(num_readings):
+        weight = hx.get_weight(1)
+        weights.append(weight)
+        time.sleep(0.1)  # Short delay between readings to stabilize
+    return sum(weights) / len(weights)
 try:
     while True:
-        time.sleep(.04) # delay between reads to stabalize
-        hx.power_down()
-        time.sleep(0.5)  # Ensure the sensor has time to power down
-        hx.power_up()
-        time.sleep(1)  # Delay between readings to allow the sensor to stabilize
-        val = hx.get_weight(5)
+        # Read data from the HX711
+        val = get_stable_weight(2)  # Get the average of 10 readings
         print(f'Weight: {val:.2f} LBS')
         
+        hx.power_down()
+        time.sleep(0.1)  # Ensure the sensor has time to power down
+        hx.power_up()
+        time.sleep(.7)  # Delay between readings to allow the sensor to stabilize
 except (KeyboardInterrupt, SystemExit):
+    # Cleanup GPIO on exit
     GPIO.cleanup()
     print("Exiting gracefully...")
