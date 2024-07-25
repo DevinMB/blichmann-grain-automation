d086d46b876a170de81a81ccb231c1a18f57ceff
Devin Butts <devinbutts@Devins-MacBook-Pro.local>
2024-07-25 14:59:12 -0400

    .

--- scale-test-2.py
+++ scale-test-2.py
@@ -3,17 +3,17 @@ from hx711 import HX711
 import time
 
 # Define GPIO pins
-SCALE_DT_PIN = 6   
-SCALE_SCK_PIN = 5  
+DT_PIN = 6   
+SCK_PIN = 5  
 
 # Initialize HX711
-hx = HX711(SCALE_DT_PIN, SCALE_SCK_PIN)
+hx = HX711(DT_PIN, SCK_PIN)
 
 # Tare to zero
 hx.tare()
 
 # Place a known weight (10 lbs = 4535.92 grams) on the load cell and get raw data
-reference_unit = -84398.99022222235 # Your previously calculated reference unit
+reference_unit = 9947.728444444423  # Your previously calculated reference unit
 hx.set_reference_unit(reference_unit)
 # For Grams : 21.926201667283962
 # For Pounds: 9944.223777777794
@@ -27,20 +27,19 @@ hx.tare()
 def get_stable_weight(num_readings=10):
     weights = []
     for _ in range(num_readings):
-        weight = hx.get_weight(3)
+        weight = hx.get_weight(1)
         weights.append(weight)
-        time.sleep(0.2)  # Short delay between readings to stabilize
+        time.sleep(0.1)  # Short delay between readings to stabilize
     return sum(weights) / len(weights)
 
 try:
     while True:
         # Read data from the HX711
-        val = get_stable_weight(1)  # Get the average of 10 readings
-        # val = hx.get_weight(1)
+        val = get_stable_weight(2)  # Get the average of 10 readings
         print(f'Weight: {val:.2f} LBS')
         
         hx.power_down()
-        time.sleep(0.2)  # Ensure the sensor has time to power down
+        time.sleep(0.1)  # Ensure the sensor has time to power down
         hx.power_up()
         time.sleep(.7)  # Delay between readings to allow the sensor to stabilize
 except (KeyboardInterrupt, SystemExit):
