670335565e24ac879dd5078cd3de9bbf5322e9d1
Devin Butts <devinmbutts@gmail.com>
2024-07-25 20:14:50 -0400

    .

--- scale-test-2.py
+++ scale-test-2.py
@@ -10,7 +10,7 @@ hx = HX711(DT_PIN, SCK_PIN)
 hx.tare()
 
 # Place a known weight (10 lbs = 4535.92 grams) on the load cell and get raw data
-reference_unit = 9944.223777777794  # Your previously calculated reference unit
+reference_unit = 19050.496  # Your previously calculated reference unit
 hx.set_reference_unit(reference_unit)
 # For Grams : 21.926201667283962
 # For Pounds: 9944.223777777794
